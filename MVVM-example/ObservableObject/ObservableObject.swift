03f1e9c210b8275d7211f05ff24b4a39f0d41650
Mikhail Kosarev <michael.kosarev@gmail.com>
2022-10-18 10:22:35 +0200

    create ObservableObject class

---
+++ MVVM-example/ObservableObject/ObservableObject.swift
@@ -0,0 +1,27 @@
+//
+//  ObservableObject.swift
+//  MVVM-example
+//
+//  Created by Mikhail on 18.10.2022.
+//
+
+import Foundation
+
+final class ObservableObject<T> {
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    private var listener: ((T) -> Void)?
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(_ listener: @escaping(T) -> Void) {
+        listener(value)
+        self.listener = listener
+    }
+}
